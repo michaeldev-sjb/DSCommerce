af673d8b60bdc91f116d92e703025e98da3a82fc
Michael Gonçalves <michaeldev.sjb@gmail.com>
2024-11-30 10:51:45 -0300

    Order

---
+++ src/test/java/com/michaeldevsjb/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaeldevsjb.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
